1a37cd469fcfec141f01d6172b3448346244edfe
Ariess202 <amjared.bd@gmail.com>
2024-07-14 18:12:39 -0500

    Se ha implementado la carga de proyectos en documentos.php y se ha modificado documentos.css y modal_doc.js (antes era modal.js) para su correcto funcionamiento

--- js/modal_doc.js
+++ js/modal_doc.js
@@ -2,13 +2,17 @@ const btnAbrirModal = document.querySelectorAll("#btn-abrir-modal");
 const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
 const modal = document.querySelector("#modal");
 const modalBackground = document.querySelector("#modal-background");
+const modalNomproyecto = document.querySelector("#modal-nomproyecto");
 
 btnAbrirModal.forEach(btn => {
-    btn.addEventListener("click", () => {
+    btn.addEventListener("click", (event) => {
+        const nomproyecto = event.target.getAttribute("data-nomproyecto");
+        modalNomproyecto.textContent = nomproyecto;
         modal.style.display = "block";
         modalBackground.style.display = "block";
     });
 });
+
 btnCerrarModal.addEventListener("click", () => {
     modal.style.display = "none";
     modalBackground.style.display = "none";
